05909cd5508ae2ea6a9d2f3aff681ddb06e73421
TatianaGuzun <tatiana.guzun@reachplc.com>
2022-03-23 15:58:55 +0000

    added userModel

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { connectToDb } from './db/helper.js';
 import { port } from './config/environment.js';
+import mongoSanitize from 'express-mongo-sanitize';
 
 const app = express();
 app.use(express.json());
+app.use(mongoSanitize());
 
 async function runServer() {
   await connectToDb();
