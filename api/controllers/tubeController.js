cfa315ba5ddc46f1310de45928db60af17e835b5
Priya <priya_93_x@hotmail.co.uk>
2022-03-24 15:17:33 +0000

    tube controller added

--- api/controllers/tubeController.js
+++ api/controllers/tubeController.js
@@ -1,24 +1,20 @@
-import 'Station' from '../models/station.js';
+import Station from '../models/station.js';
 
 const getAllStations = async (req, res, next) => {
   try {
     const station = await Station.find();
-  return res.status(200).json(station);
-}
-catch (err) {
-  next(err);
-}
+    return res.status(200).json(station);
+  } catch (err) {
+    next(err);
+  }
 };
 
 const getStationById = async (req, res) => {
   const station = await Station.findById(req.params.id);
- !station ? res.status(404) : res.status(200).json(station)
-
-
-
-}
+  !station ? res.status(404) : res.status(200).json(station);
+};
 
 export default {
   getAllStations,
   getStationById
-}
+};
