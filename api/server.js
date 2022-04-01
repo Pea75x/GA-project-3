f79609ab8bde41781cec1362a669f28236bf1e03
Edward Foulds <ed2494@googlemail.com>
2022-04-01 17:00:14 +0100

    images working

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { connectToDb } from './db/helper.js';
 import { port } from './config/environment.js';
 import mongoSanitize from 'express-mongo-sanitize';
 import router from './config/router.js';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(mongoSanitize());
 app.use('/api', router);
