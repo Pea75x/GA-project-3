03ae9043f6ee095fac4d710a1b2006da4c18d268
Priya <priya_93_x@hotmail.co.uk>
2022-05-27 15:47:15 +0100

    added backend and fixed any cloning issues

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,6 @@ dotenv.config();
 
 export const port = process.env.PORT || 3000;
 
-export const dbUrl = process.env.dbURL || 'mongodb://127.0.0.1:27017/places';
+export const dbUrl = process.env.dbURL || 'mongodb://127.0.0.1:27017/places2';
 
 export const secret = process.env.SECRET || 'apple';
